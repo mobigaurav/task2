4aa01111626abf21727810b62939ec45dbf40764
Gaurav Kumar <mobigaurav@gmail.com>
2021-04-12 11:51:23 -0400

    First commit

--- src/android/com/task2/Multiplier.java
+++ src/android/com/task2/Multiplier.java
@@ -1,10 +1,13 @@
 package com.task2;
+import android.os.Handler;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.Timer;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -15,18 +18,32 @@ public class Multiplier extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getValue")) {
             String passedValue = args.getString(0);
-            this.getValue(passedValue, callbackContext);
+            final Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+
+                @Override
+
+                public void run() {
+
+                    this.getValue(passedValue, callbackContext);
+                }
+
+                private void getValue(String passedValue, CallbackContext callbackContext) {
+                    if (passedValue != null && passedValue.length() > 0) {
+                        int num = Integer.parseInt(passedValue) * 10;
+                        callbackContext.success(num);
+                    } else {
+                        callbackContext.error("Expected one non-empty string argument.");
+                    }
+                }
+
+            }, 3000);
             return true;
+
         }
         return false;
     }
 
-    private void getValue(String passedValue, CallbackContext callbackContext) {
-        if (passedValue != null && passedValue.length() > 0) {
-             int num = Integer.parseInt(passedValue) * 10;
-            callbackContext.success(num);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+
 }
